8b0c108e0ed232d0ecee148a33b44bf1bd4dd8ad
Letapnahsor <letapnahsor@gmail.com>
2017-07-21 04:07:52 -0700

    Add files via upload

--- Day05/ex02/ft_atoi.c
+++ Day05/ex02/ft_atoi.c
@@ -3,22 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ropatel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: rliu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/03 22:39:33 by ropatel           #+#    #+#             */
-/*   Updated: 2017/07/04 23:01:03 by ropatel          ###   ########.fr       */
+/*   Created: 2017/07/03 10:38:04 by rliu              #+#    #+#             */
+/*   Updated: 2017/07/03 18:35:32 by rliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(*str)
+#include <stdio.h>
+
+void	initialize(int *i, int *nb, int *neg)
+{
+	*i = 0;
+	*nb = 0;
+	*neg = 1;
+}
+
+void	handle(char *str, int *i)
+{
+	while ((str[*i] == ' ') || (str[*i] == '\t') || (str[*i] == '\n')
+			|| (str[*i] == '\v') || (str[*i] == '\f') || (str[*i] == '\r'))
+		(*i)++;
+}
+
+int		ft_atoi(char *str)
 {
 	int i;
-	
-	i = str;
-	while (str[i] = '\0')
-	whi
+	int nb;
+	int neg;
 
-		return (0);
+	initialize(&i, &nb, &neg);
+	handle(str, &i);
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i + 1] == '-' || str[i + 1] == '+')
+			return (0);
+		if (str[i] == '-')
+			neg = -1;
+		i++;
 	}
-	return (0);
+	if (str[i] > 57 || str[i] < 48)
+		return (0);
+	while ((str[i] >= 48 && str[i] <= 57) || str[i] != '\0')
+	{
+		if (str[i] > 57 || str[i] < 48)
+			return (nb * neg);
+		nb = (nb * 10 + (str[i] - 48));
+		i++;
+	}
+	return (nb * neg);
 }
